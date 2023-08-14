cce402f649469f642a20ac6426385cfc175119b0
sammoore38 <127150634+sammoore38@users.noreply.github.com>
2023-08-14 17:30:25 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
